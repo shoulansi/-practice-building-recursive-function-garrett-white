4473362ce7294714215ec023234206173f496edb
shoulansi <garrettswhite@gmail.com>
2025-04-09 23:40:25 -0500

    final recursive

--- practice-building-recursive-function.js
+++ practice-building-recursive-function.js
@@ -1,1 +1,16 @@
+function reverseString(string){
+    if (string.length <= 1){
+        console.log(string);
+        return string;
+    } else{
+        console.log(string);
+        let reverseFunc = reverseString(string.slice(0,-1));
+        console.log(reverseFunc);
+        return string.slice(-1) + reverseFunc;
+    }
+}
 
+console.log(reverseString("Hello"));
+console.log(reverseString("This String is Backwards!"));
+console.log(reverseString("My Name Is Garrett."));
+console.log(reverseString("This is a little confusing to me."));
